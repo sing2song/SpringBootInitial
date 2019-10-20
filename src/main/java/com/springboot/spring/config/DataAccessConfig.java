d8564322680fb84462f8c8564d667ff92dd2f208
chong <chd830@naver.com>
2019-10-20 23:25:47 +0900

    jdbc, mybatis연동성공

--- src/main/java/com/springboot/spring/config/DataAccessConfig.java
+++ src/main/java/com/springboot/spring/config/DataAccessConfig.java
@@ -1,32 +1,29 @@
-//package com.springboot.spring.config;
-//import javax.sql.DataSource;
-//
-//import org.apache.ibatis.session.SqlSessionFactory;
-//import org.mybatis.spring.SqlSessionFactoryBean;
-//import org.mybatis.spring.SqlSessionTemplate;
-//import org.mybatis.spring.annotation.MapperScan;
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-//
-//@Configuration
-//@MapperScan(basePackages = "com.springboot.spring.data")
-//public class DataAccessConfig {
-//
-//    @Bean
-//    public SqlSessionFactory sqlSessionFactory(@Qualifier("dataSource") DataSource dataSource) throws Exception {
-//
-//        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-//
-//        sessionFactory.setDataSource(dataSource);
-//        sessionFactory.setMapperLocations(
-//                new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml"));
-//        return sessionFactory.getObject();
-//    }
-//
-//    @Bean
-//    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
-//        return new SqlSessionTemplate(sqlSessionFactory);
-//    }
-//}
+package com.springboot.spring.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan(basePackages = "com.springboot.spring.data")
+public class DataAccessConfig {
+
+    @Bean(name = "factory")
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactoryBean = new SqlSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource);
+        sessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml"));
+        return sessionFactoryBean.getObject();
+    }
+
+    @Bean(name = "template")
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
--- src/main/java/com/springboot/spring/config/DataSourceConfig.java
+++ src/main/java/com/springboot/spring/config/DataSourceConfig.java
@@ -1,16 +1,20 @@
-//package com.springboot.spring.config;
-//
-//import javax.sql.DataSource;
-//
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.boot.jdbc.DataSourceBuilder;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class DataSourceConfig {
-//
-//    @ConfigurationProperties(prefix="spring.datasource")
-//    public DataSource dataSource(){
-//        return DataSourceBuilder.create().build();
-//    }
-//}
+package com.springboot.spring.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Primary
+    @Bean(name ="prodDataSource")
+    @ConfigurationProperties(prefix="spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
